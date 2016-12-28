2a486d83fe624fe3502f6f7402ab3863e50b5c02
Xander <xanderdijk@hotmail.com>
2016-12-28 12:56:03 +0100

    Attempt to scrape div from source

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-//hello//
+$test = "accountant";
+$url = "https://www.stepstone.nl/5/job-search.html?stf=freeText&ns=1&qs=%5B%5D&companyID=0&cityID=0&sourceOfTheSearchField=resultlistpage%3Ageneral&searchOrigin=Resultlist_top-search&ke=".$test."&ws=&ra=30";
+$url = htmlentities($url);
+$doc = new DOMDocument();
+$doc->loadHTML($url);
+$path = new DOMXPath($doc);
+$divcontent = $path->query('//head/title');
+
+echo $divcontent->length;
