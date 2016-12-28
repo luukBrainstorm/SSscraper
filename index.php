83047ec81ea181e677e7d4a67af4921209d6db72
Xander <xanderdijk@hotmail.com>
2016-12-28 14:39:10 +0100

    filter job item header from url

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-$test = "accountant";
+$test = "software";
 $url = "https://www.stepstone.nl/5/job-search.html?stf=freeText&ns=1&qs=%5B%5D&companyID=0&cityID=0&sourceOfTheSearchField=resultlistpage%3Ageneral&searchOrigin=Resultlist_top-search&ke=".$test."&ws=&ra=30";
 $url = htmlentities($url);
-$doc = new DOMDocument();
-$doc->loadHTML($url);
-$path = new DOMXPath($doc);
-$divcontent = $path->query('//head/title');
 
-echo $divcontent->length;
+$doc = new DOMDocument;
+libxml_use_internal_errors(true);
+$doc->loadHTMLFile($url);
+$xpath = new DOMXPath($doc);
+$query = "//div[@class='jobitem__header']";
+$entries = $xpath->query($query);
+echo $entries->length."<br>";
+
+for($ii = 0; $ii < $entries->length; $ii++){
+    echo $entries->item($ii)->nodeValue."<br>";
+}
